7e9b5a83be18cb53338d0eb949c1e2dd9eaa5bfb
marianocjs10 <marianobtc2@gmail.com>
2024-09-01 06:01:56 -0300

    Se modifica src/Logica.java y src/Main.java

--- src/Logica.java
+++ src/Logica.java
@@ -6,11 +6,10 @@ public class Logica {
     private static Scanner sc = new Scanner(System.in); ///preguntar por scaner static
 
     public static void calcular(){
-
         int op;
         System.out.println("*** CALCULADORA ***");
-        System.out.println("Indique que operación desea realizar");
-        System.out.println("1-SUMA 2-RESTA 3-MULTIPLICACION 4-DIVISION 5-MODULO");
+        System.out.println("Indique que operación desea realizar\n"+
+                           "1-SUMA 2-RESTA 3-MULTIPLICACION 4-DIVISION 5-MODULO");
         op=sc.nextInt();
         double num1,num2;
         System.out.println("PRIMER N°");
@@ -38,25 +37,28 @@ public class Logica {
                 double mod = num1 % num2;
                 System.out.println("El resultado es: " + mod);
                 break;
-        };
+        }
+    }
 
-    };
-    ////REVISAR
     public static void numeroMayor(){
         System.out.println("Ingrese dos n° para compararlos");
-        double num1,num2,mayor;
+        double num1,num2;
+        double mayor=0;
         num1=sc.nextInt();
         num2=sc.nextInt();
         if(num1 > num2) {
             mayor = num1;
-        } else {
+            System.out.println("El número mayor es: " + mayor);
+        } else if (num1 < num2){
             mayor = num2;
+            System.out.println("El número mayor es: " + mayor);
+        }else{
+            System.out.println("Los números son iguales.");
         }
-        System.out.println("El número mayor es: " + mayor);
     }
-    public static void numDivisiblePorDos(){
 
-        System.out.print("INGRESE UN N°, PARA COMPROBAR SI ES DIVISIBLE POR 2: ");
+    public static void numDivisiblePorDos(){
+        System.out.println("INGRESE UN N° PARA COMPROBAR SI ES DIVISIBLE POR 2: ");
         double num = sc.nextDouble();
         if (num % 2 == 0) {
             System.out.println("El numero ingresado es divisible por 2!");
@@ -64,63 +66,64 @@ public class Logica {
             System.out.println("El numero ingresado NO es divisible por 2!");
         }
     }
+
     public static void precioConIva(){
         System.out.println("CALCULANDO IVA");
         double iva = 1.21;
+        System.out.println("Ingrese el precio sin IVA:");
         double precio = sc.nextDouble();
         DecimalFormat df = new DecimalFormat("#.00");
         System.out.println("El precio con iva es: "+ df.format(precio * iva));
     }
+
     public static void centena(){
         int numIni = 1;
         int numFin = 100;
-       /*while (numIni <= numFin ){
-            System.out.println(numIni);
+        System.out.println("Bucle WHILE ***");
+        while (numIni <= numFin ) {
+            System.out.print(" {"+numIni+"} ");
             ++numIni;
-        }*/
+        }
         int i;
-        System.out.println("IMPRIMIENDO LOS NUMEROS ***");
+        System.out.println();
+        System.out.println("Bucle FOR ***");
         for (i=1; i <= 100; ++i){
             System.out.print(" {"+ i +"} ");
         }
-        System.out.println();
     }
+
     public static void divisibles(){
         int i;
-        System.out.println("IMPRIMIENDO LOS 100 N° DIVISIBLES POR 2 Y 3");
+        System.out.println("IMPRIMIENDO LOS N° DIVISIBLES POR 2 Y 3 (entre 1 y 100)");
         for (i=1; i <= 100; ++i){
             if((i%2==0) & (i%3==0)) {
                 System.out.print(" {"+ i +"} ");
             }
         }
-        System.out.println();
     }
+
     public static void numMayorOigual(){
         int num;
-
         do {
-            System.out.print("COMPROBAR N° > 0 ");
-            System.out.println();
+            System.out.println("Ingrese un numero para comprobar si es mayor o igual a 0.");
             num = Integer.parseInt(sc.nextLine());
-        } while (num <= 0);
-
-        System.out.println("El numero ingresado: " + num + " es mayor que 0");
+        } while (num < 0);
+        if(num!=0) {
+            System.out.println("El numero ingresado: " + num + " es mayor que 0.");
+        }else{
+            System.out.println("El numero es igual a 0.");
+        }
     }
 
     public static void contrasena(){
         String contrasenaCorrecta = "contraseña", contrasena;
         int intentos = 3;
         boolean acertado = true;
-
         System.out.println("Bienvenido! Tienes 3 intentos para acceder.");
-
         while (intentos > 0 && acertado) {
-
             System.out.print("Ingrese la contraseña: ");
             contrasena = sc.nextLine();
-
             intentos--;
-
             if (contrasena.equalsIgnoreCase(contrasenaCorrecta)) {
                 System.out.println("Acceso correcto!");
                 acertado = false;
@@ -128,25 +131,20 @@ public class Logica {
                 System.out.println("Contraseña incorrecta! Te quedan " + intentos + " intentos");
             }
         }
-
         if (acertado) {
             System.out.println("Lo siento, te quedaste sin intentos!");
         }
-
     }
+
     public static void calendario(){
-
-
         System.out.println("DÍAS LABORABLES");
         System.out.println("Ingrese un n° del 1 al 7");
         int dia = Integer.parseInt(sc.nextLine());
-
         while (dia < 1 || dia > 7) {
             System.out.println("Incorrecto! Debe ingresar un numero entre 1 y 7");
             System.out.print("Ingrese nuevamente: ");
             dia = Integer.parseInt(sc.nextLine());
         }
-
         switch (dia) {
             case 1:
                 System.out.println("LUNES ES DIA LABORAL");
@@ -169,50 +167,35 @@ public class Logica {
             case 7:
                 System.out.println("DOMINGO NO ES DIA LABORAL");
                 break;
-
         }
     }
 
-
     public static void numPrimo(){
-
         System.out.println("NÚMERO PRIMO ¿?");
         System.out.println("Ingrese un n°");
         int num = Integer.parseInt(sc.nextLine());
-
         int i = 1, divisores = 0;
-
         while (i <= num) {
             if (num % i == 0) {
                 divisores++;
             }
             i++;
         }
-
         if (divisores == 2) {  // Evalúo si el num tuvo 2 divisiones (1 y si mismo)
             System.out.println("El número ingresado es primo");
         } else {
             System.out.println("El número ingresado NO es primo");
         }
-
     }
-
-
 
    public static void aleatorio(){
        int numAleatorio = Double.valueOf(Math.random() * 100).intValue();
-
        int intentos = 0, num = -1;
-
        System.out.println("Intente adivinar el numero");
-
        while (num != numAleatorio) {
-
            System.out.println("Elige un N° entre 0 y 100: ");
            num = sc.nextInt();
-
            intentos++;
-
            if (num < numAleatorio) {
                System.out.println("Es muy bajo");
            } else if (num > numAleatorio) {
--- src/Main.java
+++ src/Main.java
@@ -9,32 +9,31 @@ public class Main {
         String opcion="";
         System.out.println(" ");
         System.out.println(" **** BIENVENID@ A NUESTRA APP **** " );
-        System.out.println(" " );
+        System.out.println(" ");
         System.out.println( "Indique que operación desea realizar!");
-        System.out.println( "1- Calculadora");
-        System.out.println( "2- N° Mayor");
-        System.out.println( "3- Numero divisible por 2");
-        System.out.println( "4- Calcular IVA");
-        System.out.println( "5- Escribir del 1 al 100");
-        System.out.println( "6- N° divisibles x2 y 3 del 1 al 100");
-        System.out.println( "7- N°mayor o igual a 0");
-        System.out.println( "8- Adivine la contraseña :)");
-        System.out.println( "9- Días laborales");
-        System.out.println( "10- Decir si es un N° primo");
-        System.out.println( "11- N° aleatorio entre 0 y 100");
-        System.out.println( "0- SALIR");
+        System.out.println( "1- Calculadora\n"+
+                            "2- N° Mayor\n"+
+                            "3- Numero divisible por 2\n"+
+                            "4- Calcular IVA\n"+
+                            "5- Escribir del 1 al 100\n"+
+                            "6- N° divisibles x2 y x3 del 1 al 100\n"+
+                            "7- N°mayor o igual a 0\n"+
+                            "8- Adivine la contraseña :)\n"+
+                            "9- Días laborales\n"+
+                            "10- Decir si es un N° primo\n"+
+                            "11- N° aleatorio entre 0 y 100\n"+
+                            "0- SALIR");
         opcion=sc.nextLine();
         operaciones(opcion);
     }
-public static void delay(){
-    try {
-        Thread.sleep(1000);
-    } catch (InterruptedException e) {
-        e.printStackTrace();
+    public static void delay(){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-}
     public static void operaciones(String opcion){
-      
             switch (opcion) {
                 case "1":
                     Logica.calcular();
@@ -45,52 +44,60 @@ public static void delay(){
                     Logica.numeroMayor();
                     delay();
                     menu();
+                    break;
                 case "3":
                     Logica.numDivisiblePorDos();
                     delay();
                     menu();
+                    break;
                 case "4":
                     Logica.precioConIva();
                     delay();
                     menu();
+                    break;
                 case "5":
                     Logica.centena();
                     delay();
                     menu();
+                    break;
                 case "6":
                     Logica.divisibles();
                     delay();
                     menu();
+                    break;
                 case "7":
                     Logica.numMayorOigual();
                     delay();
                     menu();
+                    break;
                 case "8":
                     Logica.contrasena();
                     delay();
                     menu();
+                    break;
                 case "9":
                     Logica.calendario();
                     delay();
                     menu();
+                    break;
                 case "10":
                     Logica.numPrimo();
                     delay();
                     menu();
+                    break;
                 case "11":
                     Logica.aleatorio();
                     delay();
                     menu();
-                case "0":
-                    System.out.println("***Usted salió de al APLICACIÓN ***");
                     break;
-                    default:
-                        System.out.println("Caracter no valido, intente de nuevo");
-                        delay();
-                        menu();
+                case "0":
+                    System.out.println("***Usted salió de la APLICACIÓN ***");
+                    break;
+                default:
+                    System.out.println("Caracter no valido, intente de nuevo");
+                    delay();
+                    menu();
+                    break;
             }
     }
-
-
-
 }
